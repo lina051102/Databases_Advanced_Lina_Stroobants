e4d9010f75544995de54ba9dc6f59abccb0163ea
lina051102 <lina.stroobants@hotmail.com>
2022-03-19 13:25:36 +0100

    Bash file aangepast

--- BashScraper.sh
+++ BashScraper.sh
@@ -1,2 +1,19 @@
-## test
-echo "test"
+# ------------------ Bash file for scraper ------------------
+
+# Clearing window
+clear
+
+# Starting script
+echo "Starting script"
+
+# Starting MongoDB server
+mongod
+
+# Starting shell
+mongo
+
+# Ending script
+echo "Ending script"
+
+# Printing bash file
+cat BashScraper.sh
