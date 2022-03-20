cf1f7a84d157a8b92b37ef72a4ef258950d8aaea
lina051102 <lina.stroobants@hotmail.com>
2022-03-20 11:04:35 +0100

    Bash file Redis afgewerkt

--- BashRedis.sh
+++ BashRedis.sh
@@ -1,1 +1,17 @@
 # ------------------ Bash file for Redis ------------------
+# Install Ubuntu in Microsoft Store
+
+# Install Remote - WSL in Visual Studio
+
+# Config Ubuntu in cmd
+wslconfig /setdefault Ubuntu
+
+# Install Redis in Visual Studio
+## Open WSL terminal
+sudo apt-get install redis
+
+redis-server
+
+redis-server --port 6380
+
+redis-cli ping
